71f207bed1e51332b2b4c8783ebb85074fb1565b
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-21 20:43:08 +0000

    create _printf function

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _printf - prints Anything
+ * @format: list of args types passed to the function
+ * Return: the number of charaters printed
+ * Description: this function prints anything
+ */
+int _printf(const char *format, ...)
+{
+	int a;
+
+	a = strlen(format);
+	return (a);
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif /*MAIN_H*/
