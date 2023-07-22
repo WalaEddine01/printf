1a1d3baa227545d9ee72db8ca597a63bd1845d61
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-22 12:04:56 +0000

    i made the option for character and string and percetage

--- _printf.c
+++ _printf.c
@@ -7,9 +7,43 @@
  */
 int _printf(const char *format, ...)
 {
-	int a;
+	int a, b, i;
+	va_list arg;
+	char *s;
 
+	i = 0;
 	a = strlen(format);
-	write(1, format, a);
+	va_start(arg, format);
+	while(format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					b = va_arg(arg, int);
+					write(1, &b, 1);
+					break;
+				case 's':
+					s = va_arg(arg, char *);
+					write(1, s, strlen(s));
+					break;
+				case '%':
+					write(1, &format[i], 1);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			i++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			i++;
+		}
+	}
+	va_end(arg);
 	return (a);
 }
---
+++ wal.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len2;
+
+    len2 = _printf("Let's try to printf a simple sentence.\n");
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    return (0);
+}
